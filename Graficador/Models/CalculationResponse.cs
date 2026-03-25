0e8295409e473310b2ffdfddd67e5602b6e72a05
luchorivardo <128877604+luchorivardo@users.noreply.github.com>
2026-03-25 19:48:00 -0300

    .

--- Graficador/Models/CalculationResponse.cs
+++ Graficador/Models/CalculationResponse.cs
@@ -2,5 +2,8 @@
 {
     public class CalculationResponse
     {
+        public List<IterationPoint> Iterations { get; set; } = new();
+        public string Root { get; set; }
+        public string GgbCommand { get; set; } // Comando para GeoGebra
     }
 }
